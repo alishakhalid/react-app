b10fe2fda19682b1e547e4e6bc9d2386e503c30d
alishakhalid <69908299+alishakhalid@users.noreply.github.com>
2021-09-23 10:22:04 +0200

    added card in hompepage and css

--- mywebsite/src/homepage.tsx
+++ mywebsite/src/homepage.tsx
@@ -7,6 +7,17 @@ export default function Homepage() {
   return (
     <div>
       <h1>Anu used this proj...</h1>
+      <Card>
+        <Card.Img
+          className="card-img-top"
+          src="https://i.pinimg.com/originals/39/a5/86/39a586df6f64c9c87098c3108e134eba.jpg"
+        />
+        <Card.Body>
+          <Card.Title>Donuts</Card.Title>
+          <Card.Text>Delicious Donuts mady by ANUUU!</Card.Text>
+          <Button variant="primary">Surprise</Button>
+        </Card.Body>
+      </Card>
     </div>
   );
 }
