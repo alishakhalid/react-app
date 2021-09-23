b51ebce4e36b5a929e0ea1c8d36c06c547257faa
alishakhalid <69908299+alishakhalid@users.noreply.github.com>
2021-09-23 10:29:57 +0200

    replaced card with carousel

--- mywebsite/src/homepage.tsx
+++ mywebsite/src/homepage.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button, Card } from "react-bootstrap";
+import { Carousel } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/homepage.css";
 
@@ -7,17 +7,19 @@ export default function Homepage() {
   return (
     <div>
       <h1>Anu used this proj...</h1>
-      <Card>
-        <Card.Img
-          className="card-img-top"
-          src="https://i.pinimg.com/originals/39/a5/86/39a586df6f64c9c87098c3108e134eba.jpg"
-        />
-        <Card.Body>
-          <Card.Title>Donuts</Card.Title>
-          <Card.Text>Delicious Donuts mady by ANUUU!</Card.Text>
-          <Button variant="primary">Surprise</Button>
-        </Card.Body>
-      </Card>
+      <Carousel>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="https://i.pinimg.com/originals/e4/4f/38/e44f3858bbcfe009df4c7fbad1f68edc.jpg"
+            alt="First slide"
+          />
+          <Carousel.Caption>
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
     </div>
   );
 }
