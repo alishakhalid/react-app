30748eda7357f8995004fd94acab97017f2a0311
alishakhalid <69908299+alishakhalid@users.noreply.github.com>
2021-09-23 09:50:42 +0200

    added card with image

--- mywebsite/src/homepage.tsx
+++ mywebsite/src/homepage.tsx
@@ -1,10 +1,23 @@
-import React from 'react'
+import React from "react";
+import { Button, Card } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./css/homepage.css";
 
 export default function Homepage() {
-    return (
-        <div>
-            <h1>Anu used this proj...</h1>
-            
-        </div>
-    )
+  return (
+    <div>
+      <h1>Anu used this proj...</h1>
+      <Card>
+        <Card.Img
+          className="card-img-top"
+          src="https://i.pinimg.com/originals/39/a5/86/39a586df6f64c9c87098c3108e134eba.jpg"
+        />
+        <Card.Body>
+          <Card.Title>Donuts</Card.Title>
+          <Card.Text>Delicious Donuts mady by ANUUU!</Card.Text>
+          <Button variant="primary">Surprise</Button>
+        </Card.Body>
+      </Card>
+    </div>
+  );
 }
